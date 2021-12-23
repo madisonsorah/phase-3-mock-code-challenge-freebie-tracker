f62c01207be180c352b9545b0958173c7f85c08a
madison sorah <madisonsorah@gmail.com>
2021-12-22 23:21:47 -0500

    Completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,16 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
+Freebie.create(item_name: "Keychain", value: 2, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Hat", value: 10, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Shirt", value: 20, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Tote", value: 5, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Mousepad", value: 5, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Sticker", value: 1, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Magnet", value: 1, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Figurine", value: 15, company_id: rand(1..4), dev_id: rand(1..4))
+Freebie.create(item_name: "Pen", value: 2, company_id: rand(1..4), dev_id: rand(1..4))
+
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
